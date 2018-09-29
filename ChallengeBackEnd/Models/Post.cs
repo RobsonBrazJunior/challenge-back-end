6ebd19ec12d5afbd94bed723da17c1e6d1d043a4
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-29 15:30:03 -0400

    Criada classe Model Post.

---
+++ ChallengeBackEnd/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChallengeBackEnd.Models
+{
+    public class Post
+    {
+        public long? PostID { get; set; }
+        public string Titulo { get; set; }
+        public string Conteudo { get; set; }
+        public int Views { get; set; }
+        public int Likes { get; set; }
+    }
+}
