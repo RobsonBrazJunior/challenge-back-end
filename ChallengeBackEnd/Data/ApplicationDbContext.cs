eaf9d17bf7ead77395623c42f0a57e91bf706f92
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-29 15:56:59 -0400

    Adicionada propriedade no contexto para mapeamento da classe Post

--- ChallengeBackEnd/Data/ApplicationDbContext.cs
+++ ChallengeBackEnd/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ChallengeBackEnd.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,7 @@ namespace ChallengeBackEnd.Data
             : base(options)
         {
         }
+
+        public DbSet<Post> Posts { get; set; }
     }
 }
