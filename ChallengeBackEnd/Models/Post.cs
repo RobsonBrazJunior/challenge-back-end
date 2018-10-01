d7f59217aa01934fd16e2070b995c3ff29bd6b9b
Junior Braz <robsonbrazjunior@gmail.com>
2018-10-01 06:26:14 -0400

    Organizando e finalizando projeto.

--- ChallengeBackEnd/Models/Post.cs
+++ ChallengeBackEnd/Models/Post.cs
@@ -5,15 +5,10 @@ namespace ChallengeBackEnd.Models
     public class Post
     {
         public int PostID { get; set; }
-
         public string Titulo { get; set; }
-
         public string Resumo { get; set; }
-
         public string Conteudo { get; set; }
-
         public int Views { get; set; }
-
         public int Likes { get; set; }
 
         [NotMapped]
